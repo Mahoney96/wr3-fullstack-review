0109099c591602cb4ab7da97caf5916f6153ccbf
Mahoney96 <cmahoney966@gmail.com>
2020-08-13 22:58:14 -0700

    Needs more code updated

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import "./Header.css";
+import {Link} from "react-router-dom";
+
+const Header = () => {
+    return <header>
+        <div className='logo-container'>
+            <img alt='logo' src="https://www.clipartmax.com/png/middle/101-1015647_i-made-yall-a-snoo-reddit-logo-png.png" className='logo'/>
+        </div>
+        <h1>Schmeddit</h1> 
+            <nav className='navigation'>
+                <ul>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/front_page">Front Page</Link></li>
+                    <li><Link to=" " >Logout</Link></li>
+                </ul>
+            </nav>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,8 @@
+  
+import React from 'react';
+
+const Profile = () => {
+    return <div> This is the Profile page</div>
+}
+
+export default Profile;
